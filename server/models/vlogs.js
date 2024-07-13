ea368ff6eaec046ba7362b35b70247e778b25433
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-07-13 16:34:56 +0530

    Update vlogs.js

--- server/models/vlogs.js
+++ server/models/vlogs.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
 const vlogSchema = new mongoose.Schema({
-
+    
+    category:{
+        type: String,
+        required: true
+    },
+    Title:{
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+       
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    cover_image : {
+        type: String,
+        
+    }
 })
 const Vlogs = mongoose.model('Vlogs', vlogSchema);
 module.exports = Vlogs;
