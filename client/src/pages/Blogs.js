6f6ae808e1478447fa167bb407262f29956004c5
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-07-15 15:54:04 +0530

    modify blog

--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import NavBar from '../component/NavBar'
 import BlogCompo from '../component/BlogCompo'
+import BGG from '../images/BGG.jpg'
 
 
 export default function Blogs() {
   return (
-    <div>
-        <div>
+    <div className="min-h-screen bg-right bg-cover no-repeat bg- lg-h-screen" style={{ backgroundImage: `url(${BGG})`,}}>
+        <div className='bg-white bg-opacity-10'>
             <NavBar/>
         </div>
       <div>
