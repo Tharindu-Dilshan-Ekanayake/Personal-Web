e92ce3f1263e179137b3ffbb73311c226deab70f
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-07-15 09:02:18 +0530

    Update BlogCompo.js

--- client/src/component/BlogCompo.js
+++ client/src/component/BlogCompo.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import BG from '../images/BG.jpg';
+import React from 'react'
 
 export default function BlogCompo() {
   return (
-    <div className="relative flex items-center justify-center min-h-screen overflow-hidden">
-      {/* Background image with reduced opacity */}
-      <div 
-        className="absolute inset-0 bg-center bg-no-repeat bg-cover opacity-20"
-        style={{ backgroundImage: `url(${BG})` }}
-      ></div>
-      
-     
-      
-      {/* Content */}
-      <div className="relative z-10 text-white">
-        <h1 className="text-4xl font-bold">Blogs</h1>
-      </div>
+    <div>
+      <h1>Blogsss</h1>
     </div>
-  );
+  )
 }
